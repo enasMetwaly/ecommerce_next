b663430084158aa1a602c22cdc3a87e8c35ef56b
enasMetaly <enasmetwally99@gmail.com>
2024-04-19 14:26:59 +0200

    Add header and footer components to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header/>
       <body className={inter.className}>{children}</body>
+      <Footer/>
     </html>
   );
 }
