0ee6f51fd430e6694f7fbc838146c93e1d5f9e4c
enasMetaly <enasmetwally99@gmail.com>
2024-04-24 01:37:57 +0200

    Displayed products using the ProductItem component

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -1,1 +1,15 @@
-g
+import ProductItem from "./ProductItem";
+
+const ProductList = ({ productList }) => {
+    return (
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+        {productList.map((item) => (
+          <div>
+            <ProductItem  product={item} key={item.id}/>
+          </div> 
+        ))}
+      </div>
+    );
+  };
+
+  export default ProductList;
