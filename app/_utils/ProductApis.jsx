20836a018692cc245859b34f3d02f26f9aaa503f
enasMetaly <enasmetwally99@gmail.com>
2024-04-25 11:19:23 +0200

    add getProductById function including  'id' parameter for product details retrieval.

--- app/_utils/ProductApis.jsx
+++ app/_utils/ProductApis.jsx
@@ -1,7 +1,11 @@
 const { default: axiosClient } = require("./axiosClient");
 
-const getLatestProduct=()=>axiosClient.get('api/products?populate=*')
+const getLatestProduct = () => axiosClient.get('api/products?populate=*');
+
+// Corrected getProductById with 'id' parameter
+const getProductById = (id) => axiosClient.get(`api/products/${id}?populate=*`);
 
 export default {
-    getLatestProduct
-}
+    getLatestProduct,
+    getProductById
+};
