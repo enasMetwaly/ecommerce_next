781d1fdd1225b5359568d83735a8b7d03a804324
enasMetaly <enasmetwally99@gmail.com>
2024-04-25 11:26:14 +0200

    added ProductBanner component to display a product's  image

---
+++ app/product-details/[productId]/_components/ProductBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+const ProductBanner = ({product}) => {
+  return (
+    <div >
+        <Image 
+            src={product?.attributes?.banner?.data?.attributes?.url}
+            alt='product-detail-banner'
+            width={400}
+            height={400}
+            className='rounded-lg'
+        />
+    </div>
+  )
+}
+
+export default ProductBanner
