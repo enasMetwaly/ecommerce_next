522b0b909fe3fb4b7ecbbab351cb447f12620d11
enasMetaly <enasmetwally99@gmail.com>
2024-04-26 23:48:16 +0300

    Update Footer component to use Clerk's useUser hook for conditional rendering

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,7 +1,14 @@
+'use client'
+
 import React from 'react'
+import { useUser } from '@clerk/nextjs';
+
 
 const Footer = () => {
-  return (
+  const { user } = useUser();
+
+  return user && (
+
     <div>
       
     </div>
