e2d2307f40ac34755a92d9880363bd70c3146d58
enasMetaly <enasmetwally99@gmail.com>
2024-04-26 13:13:56 +0300

    Add getProductByCategory to axios client module to fetch products based on category

--- app/_utils/ProductApis.jsx
+++ app/_utils/ProductApis.jsx
@@ -5,7 +5,14 @@ const getLatestProduct = () => axiosClient.get('api/products?populate=*');
 // Corrected getProductById with 'id' parameter
 const getProductById = (id) => axiosClient.get(`api/products/${id}?populate=*`);
 
+
+//product recommendation
+// Corrected API endpoint
+const getProductByCategory = (category) =>
+  axiosClient.get(`/api/products?filters[category][$eq]=${category}&populate=*`);
+
 export default {
     getLatestProduct,
-    getProductById
+    getProductById,
+    getProductByCategory
 };
