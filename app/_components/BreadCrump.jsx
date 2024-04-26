04fdf253a4783357edb59ebafb65f01a54e37e4a
enasMetaly <enasmetwally99@gmail.com>
2024-04-26 13:22:58 +0300

    Update BreadCrump to use Next.js Link and dynamic path segments

--- app/_components/BreadCrump.jsx
+++ app/_components/BreadCrump.jsx
@@ -1,13 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
-const BreadCrump = () => {
+const BreadCrump = ({path}) => {
     return (
 
         <nav aria-label="Breadcrumb" className="flex">
             <ol className="flex overflow-hidden rounded-lg border border-gray-200 text-gray-600">
                 <li className="flex items-center">
-                    <a
-                        href="#"
+                    <Link
+                        href="/"
                         className="flex h-10 items-center gap-1.5 bg-gray-100 px-4 transition hover:text-gray-900"
                     >
                         <svg
@@ -26,7 +27,7 @@ const BreadCrump = () => {
                         </svg>
 
                         <span className="ms-1.5 text-xs font-medium"> Home </span>
-                    </a>
+                    </Link>
                 </li>
 
                 <li className="relative flex items-center">
@@ -39,8 +40,23 @@ const BreadCrump = () => {
                         href="#"
                         className="flex h-10 items-center bg-white pe-4 ps-8 text-xs font-medium transition hover:text-gray-900"
                     >
-                        Shirts
+                    {path?.split('/')[1]}
                     </a>
+
+                </li>
+                <li className="relative flex items-center">
+                    <span
+                        className="absolute inset-y-0 -start-px h-10 w-4 bg-gray-100 [clip-path:_polygon(0_0,_0%_100%,_100%_50%)] rtl:rotate-180"
+                    >
+                    </span>
+
+                    <a
+                        href="#"
+                        className="flex h-10 items-center bg-white pe-4 ps-8 text-xs font-medium transition hover:text-gray-900"
+                    >
+                    {path?.split('/')[2]}
+                    </a>
+                    
                 </li>
             </ol>
         </nav>
