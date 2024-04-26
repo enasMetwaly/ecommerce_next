2d8d5f66bcd1919a6e7903cc2b4fb9213da9c408
enasMetaly <enasmetwally99@gmail.com>
2024-04-26 23:27:06 +0300

    Add Clerk middleware for public routes and route matching in Next.js

---
+++ middleware.js
@@ -0,0 +1,10 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+  publicRoutes:['/','/product-details/(.*)']
+
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
