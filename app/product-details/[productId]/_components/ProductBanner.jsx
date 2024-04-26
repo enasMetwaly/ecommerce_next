546a2143cfa39169f5abe3a638824e7b612446f5
enasMetaly <enasmetwally99@gmail.com>
2024-04-26 14:30:38 +0300

    Updated ProductBanner component to include loading animation until product loading data

--- app/product-details/[productId]/_components/ProductBanner.jsx
+++ app/product-details/[productId]/_components/ProductBanner.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
-const ProductBanner = ({product}) => {
-  return (
-    <div >
-        <Image 
-            src={product?.attributes?.banner?.data?.attributes?.url}
-            alt='product-detail-banner'
-            width={400}
-            height={400}
-            className='rounded-lg'
-        />
-    </div>
-  )
-}
+import React from 'react';
+import Image from 'next/image';
 
-export default ProductBanner
+const ProductBanner = ({ product }) => {
+  return (
+    <div>
+      {product ? (
+        <Image
+          src={product?.attributes?.banner?.data?.attributes?.url}
+          alt="product-detail-banner"
+          width={400}
+          height={400}
+          className="rounded-lg"
+        />
+      ) : (
+        <div className="w-[400px] h-[225px] bg-slate-200 rounded-lg animate-pulse"></div>
+      )}
+    </div>
+  );
+};
+
+export default ProductBanner;
