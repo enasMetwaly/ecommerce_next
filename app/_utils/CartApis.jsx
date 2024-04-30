9ad9c3c8fb4c8c9cf6ef41901425b5ae09580923
enasMetaly <enasmetwally99@gmail.com>
2024-04-30 16:18:46 +0300

    add deleteCartItem function  to enable cart item deletion through an API endpoint

--- app/_utils/CartApis.jsx
+++ app/_utils/CartApis.jsx
@@ -5,7 +5,12 @@ const addToCart=(payload)=>axiosClient.post('api/carts',payload)
 //to avoid cart items dissapear after refresh you should call api from database: take email to get user data
 const getUserCartItems=(email)=>axiosClient.get(`api/carts?populate[products][populate]=banner&filter[email][$eq]=${email}`)
 
+
+//delete cart item 
+const deleteCartItem=(id)=>axiosClient.delete(`api/carts/${id}`)
+
 export default {
     addToCart,
-    getUserCartItems
+    getUserCartItems,
+    deleteCartItem
 }
