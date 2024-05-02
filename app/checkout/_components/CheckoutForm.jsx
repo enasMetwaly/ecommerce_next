af1f58ee045c7d6f85769932af8894652bd5ea98
enasMetaly <enasmetwally99@gmail.com>
2024-05-02 16:24:42 +0300

    Update CheckoutForm: Integrate CartContext, OrderApis, and CartApis for order creation and cart item deletion

--- app/checkout/_components/CheckoutForm.jsx
+++ app/checkout/_components/CheckoutForm.jsx
@@ -1,12 +1,20 @@
 
+import { CartContext } from '@/app/_context/CartContext';
+import { useUser } from '@clerk/nextjs';
 import { useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
 import { useState } from 'react';
+import { useContext } from 'react';
+import OrderApis from '@/app/_utils/OrderApis';
+import CartApis from '@/app/_utils/CartApis';
 
-const CheckoutForm = ({amount}) => {
+const CheckoutForm = ({ amount }) => {
     const stripe = useStripe();
     const elements = useElements();
     const [loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
+    //for order
+    const { cart, setCart } = useContext(CartContext)
+    const { user } = useUser()
 
     const handleSubmit = async (event) => {
         event.preventDefault(); // Prevent default form submission
@@ -20,6 +28,9 @@ const CheckoutForm = ({amount}) => {
             setLoading(false);
             setErrorMessage(error.message);
         };
+
+        //call create order function beforr trigger validation
+        createOrder()
 
         // Trigger form validation and wallet collection
         const { error: submitError } = await elements.submit();
@@ -47,7 +58,7 @@ const CheckoutForm = ({amount}) => {
                 clientSecret,
                 elements,
                 confirmParams: {
-                    return_url: "http://localhost:3000/payment-confirm", 
+                    return_url: "http://localhost:3000/payment-confirm",
                 },
             });
 
@@ -61,6 +72,34 @@ const CheckoutForm = ({amount}) => {
             handleError(error);
         }
     };
+
+    //order function
+    const createOrder = () => {
+        let productIds = []
+        cart.forEach(el => {
+            productIds.push(el?.product?.id)
+
+        });
+        const data = {
+            data: {
+                email: user.primaryEmailAddress.emailAddress,
+                username: user.fullName,
+                amount,
+                //  products contain ids of cart items that user will order i obtained ubove
+                products: productIds
+
+
+            }
+        }
+        OrderApis.createOrder(data).then((res) => {
+            if (res) {
+                cart.forEach(el => {
+                    CartApis.deleteCartItem(el?.id)
+                })
+            }
+
+        }).then(result => { })
+    }
 
     return (
         <form onSubmit={handleSubmit}>
